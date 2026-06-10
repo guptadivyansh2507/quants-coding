dbf68d6f719b5d38bf4b7d5eb9bfb46942345613
Divyansh Gupta <guptadivyansh2507@gmail.com>
2026-06-10 23:35:30 +0530

    Add initial variable declarations and operations in day38.py

---
+++ day38.py
@@ -0,0 +1,26 @@
+a = 1 # a is a integer
+b = 5.22 # b is a float
+c = "Harry" # c is a string
+d = False # d is a boolean variable
+e = None # e is a none variable
+a = 34 
+b = 4
+c = a+b
+print(c)
+
+a = 4-3
+b = 6
+b += 3 
+print(a+b)
+d = 5>4
+print(d)
+
+print("True or True is", True or True)
+print("False or False is", False or False)
+print("False or True is ", False or True)
+print("True or False is", True or False)
+print(not(False))
+
+a = 31.2
+t = type(a)
+print(t)
