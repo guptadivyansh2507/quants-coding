e0a9689731db722b2f7dfada1b44ad7f78868a2b
Divyansh Gupta <guptadivyansh2507@gmail.com>
2026-06-11 22:51:09 +0530

    Add dynamic variable assignment example

---
+++ day39.py
@@ -0,0 +1,7 @@
+# Dynamic variable assignment
+user_name = "Alex"       # String (text)
+user_age = 25            # Integer (whole number)
+user_height = 5.9        # Float (decimal number)
+is_learning = True       # Boolean (True/False)
+
+print(f"{user_name} is {user_age} years old.")
